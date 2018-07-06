eb613b9cd79aabd0cdd0a37af5dffc5d6f0c016f
Anh Bach Quoc <aquoc@nomosphere.fr>
2018-07-06 11:20:55 +0200

    added mock backend request unit test for api service via interceptor

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { fakeBackendProvider} from '../interceptors/fakeBackend.interceptor';
 import { ApiService } from './api.service';
 import {HttpClientModule} from '@angular/common/http';
 
@@ -7,11 +7,16 @@ describe('ApiService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      providers: [ApiService]
+      providers: [ApiService, fakeBackendProvider]
     });
   });
 
   it('should be created', inject([ApiService], (service: ApiService) => {
     expect(service).toBeTruthy();
+  }));
+  it('should request and get cod:"200" as answer', inject([ApiService], (service: ApiService) => {
+    service.getForecast('testCity', 'testCc').subscribe(response => {
+      expect(response['cod']).toBe('200');
+    });
   }));
 });
